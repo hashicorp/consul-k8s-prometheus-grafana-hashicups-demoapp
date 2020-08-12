c41365dae34a2c7a0d775a5e10ec55777c87818d
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2020-08-12 15:36:24 -0400

    Initial commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+helm install -f helm/consul-values.yaml consul hashicorp/consul --version "0.23.1" --wait
+
+helm install -f helm/prometheus-values.yaml prometheus stable/prometheus --version "11.7.0" --wait
+
+helm install -f helm/grafana-values.yaml grafana stable/grafana --version "5.3.6" --wait && \
+
+kubectl apply -f app
