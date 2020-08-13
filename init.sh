abaa3978171158bda76c6f6ee08644904766f764
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2020-08-13 08:44:58 -0400

    Removed double ampersand.

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ helm install -f helm/consul-values.yaml consul hashicorp/consul --version "0.23.
 
 helm install -f helm/prometheus-values.yaml prometheus stable/prometheus --version "11.7.0" --wait
 
-helm install -f helm/grafana-values.yaml grafana stable/grafana --version "5.3.6" --wait &&
+helm install -f helm/grafana-values.yaml grafana stable/grafana --version "5.3.6" --wait
 
 kubectl apply -f app
